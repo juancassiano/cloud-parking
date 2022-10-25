769dde001c177af3ba2d2b5e57da7c21d2691280
Juan Cassiano <juancassiano@hotmail.com>
2022-10-25 16:01:57 -0300

    Estrutura Inicial

--- src/main/java/one/digitalinnovation/parking/controller/HomeController.java
+++ src/main/java/one/digitalinnovation/parking/controller/HomeController.java
@@ -1,2 +1,15 @@
-package one.digitalinnovation.parking.controller;public class HomeController {
+package one.digitalinnovation.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping()
+    public String Home(){
+        return "Welcome";
+    }
 }
